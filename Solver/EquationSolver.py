0856bfcc8ef602212be9c24873bf8ff63abfc036
Dino <dinovin7@gmail.com>
2018-08-24 21:08:35 -0500

    Enhanced the style of the code

--- Solver/EquationSolver.py
+++ Solver/EquationSolver.py
@@ -4,13 +4,13 @@ from random import randint
 from time import sleep
 import tkinter as tk
 
-# a = tkinter.Checkbutton
-
 # Definitions:
 
 
 def cubic_root(x):
     return pow(x, 1/3) if x >= 0 else -pow(-x, 1/3)
+
+# Main class:
 
 
 class Solver:
@@ -19,16 +19,10 @@ class Solver:
         self.degree = 0
         self.solution = 0
         self.h = 1e-7
-        self.a = 0
-        self.b = 0
-        self.c = 0
-        self.d = 0
-        self.e = 0
-        self.f = 0
+        self.a, self.b, self.c, self.d, self.e, self.f = 0, 0, 0, 0, 0, 0
+        self.initA, self.initB, self.initC, self.initD, self.initE, self.initF = 0, 0, 0, 0, 0, 0
         self.s5 = 0
-        self.C = 0
-        self.D = 0
-        self.E = 0
+        self.C, self.D, self.E = 0, 0, 0
         self.W = [1e-7]
         self.X = 0
         self.Y = 0
@@ -47,18 +41,18 @@ class Solver:
         self.degree = int(input('Enter the degree of the polynomial equation (1-5):\n'))
         while self.degree > 5 or self.degree < 1:
             self.degree = int(input('The degree should be either 1, 2, 3, 4, or 5. Enter a new degree:\n'))
-        self.a = int(input('Enter a: '))
+        self.initA = self.a = int(input('Enter a: '))
         while self.a == 0:
-            self.a = int(input('a cannot be 0. Enter a new value: '))
-        self.b = int(input('Enter b: '))
+            self.initA = self.a = int(input('a cannot be 0. Enter a new value: '))
+        self.initB = self.b = int(input('Enter b: '))
         if self.degree >= 2:
-            self.c = int(input('Enter c: '))
+            self.initC = self.c = int(input('Enter c: '))
         if self.degree >= 3:
-            self.d = int(input('Enter d: '))
+            self.initD = self.d = int(input('Enter d: '))
         if self.degree >= 4:
-            self.e = int(input('Enter e: '))
+            self.initE = self.e = int(input('Enter e: '))
         if self.degree == 5:
-            self.f = int(input('Enter f: '))
+            self.initF = self.f = int(input('Enter f: '))
 
     def fun(self, x):
         return 0 if self.degree < 5 else self.a * x ** 5 + self.b * x ** 4 + self.c * x ** 3 + self.d * x ** 2 +\
@@ -108,12 +102,11 @@ class Solver:
              (-self.b + self.Z - sqrt(-self.Z ** 2 + self.C - self.D / self.Z)) / self.a / 4,
              (-self.b + self.Z + sqrt(-self.Z ** 2 + self.C - self.D / self.Z)) / self.a / 4,
              self.s5 + 0j], key=abs)[::-1]
-        for i in range(self.degree):
-            print(self.format_solution(self.solution[i]))
+        for k in range(self.degree):
+            print(self.format_solution(self.solution[k]))
 
 
 # EquationSolver:
-
 
 solver = Solver()
 Solver.welcome_message()
@@ -124,42 +117,44 @@ if solver.degree > 4:
 solver.calculate_solutions()
 solver.print_solutions()
 
+# User interface
 
 root = tk.Tk()
 root.title("Equation Solver")
 
-label = tk.Label(root, fg="blue")
-label.pack()
-degrees_string = "";
+degrees_label = tk.Label(root, fg="blue")
+degrees_label.pack()
+
+degrees_string = "Degree: " + str(solver.degree) + "\nEquation coefficients:\n"
 for i in range(0, solver.degree+1):
     degrees_string += chr(ord('a')+i) + " = "
-    number = "";
+    number = ""
     if i == 0:
-        number += str(solver.a)
+        number += str(solver.initA)
     if i == 1:
-        number += str(solver.b)
+        number += str(solver.initB)
     if i == 2:
-        number += str(solver.c)
+        number += str(solver.initC)
     if i == 3:
-        number += str(solver.d)
+        number += str(solver.initD)
     if i == 4:
-        number += str(solver.e)
+        number += str(solver.initE)
     if i == 5:
-        number += str(solver.f)
+        number += str(solver.initF)
     degrees_string += number
     if i != solver.degree:
         degrees_string += ", "
 
-label.config(text=degrees_string)
+degrees_label.config(text=degrees_string)
 
-label2 = tk.Label(root, fg="red")
-label2.pack()
+solutions_label = tk.Label(root, fg="red")
+solutions_label.pack()
 solutions = "Solutions:\n"
 for i in range(0, solver.degree):
     solutions += solver.format_solution(solver.solution[i])
     solutions += "\n"
 
-label2.config(text=solutions)
+solutions_label.config(text=solutions)
 
 button = tk.Button(root, text="Exit", width=25, command=root.destroy)
 button.pack()
